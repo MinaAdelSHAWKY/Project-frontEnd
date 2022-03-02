660d2585c841f7564be9d96a38dd4bad283c2db2
MinaAdelSHAWKY <minaa8956@gmail.com>
2022-03-02 20:42:57 +0200

    first ini

---
+++ Final project front end/Project.js
@@ -0,0 +1,90 @@
+function myFunction() {
+    var m = document.getElementById("articles_10");
+      var f = document.getElementById("flex_10");
+      var y =  document.getElementById("minus");
+      var z =  document.getElementById("plus");
+      var x = document.getElementById("para_of_10");
+      if (x.style.display === "none") {
+          x.style.display = "block";
+          y.style.display= "inline";
+          z.style.display= "none";
+        f.style.color= "red";
+        m.style.color= "red";
+        m.style.border= "2px solid blue";
+      } else {
+        x.style.display = "none";
+        y.style.display= "none";
+        z.style.display= "inline";
+        f.style.removeProperty("color");
+        m.style.removeProperty("color");
+        m.style.removeProperty("border");
+      }
+    }
+    function myFunction_1() {
+      var m = document.getElementById("articles_10_1");
+      var f = document.getElementById("flex_10_1");
+      var y =  document.getElementById("minus_1");
+      var z =  document.getElementById("plus_1");
+      var x = document.getElementById("para_of_10_1");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display= "inline";
+        z.style.display= "none";
+        f.style.color= "red";
+        m.style.color= "red";
+        m.style.border= "2px solid blue";
+  
+      } else {
+        x.style.display = "none";
+        z.style.display= "inline";
+        y.style.display= "none";
+        f.style.removeProperty("color");
+        m.style.removeProperty("color");
+        m.style.removeProperty("border");
+      }
+    }
+    function myFunction_2() {
+      var m = document.getElementById("articles_10_2");
+      var f = document.getElementById("flex_10_2");
+      var y =  document.getElementById("minus_2");
+      var z =  document.getElementById("plus_2");
+      var x = document.getElementById("para_of_10_2");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display= "inline";
+        z.style.display= "none";
+        f.style.color= "red";
+        m.style.color= "red";
+        m.style.border= "2px solid blue";
+      } else {
+        x.style.display = "none";
+        z.style.display= "inline";
+        y.style.display= "none";
+        f.style.removeProperty("color");
+        m.style.removeProperty("color");
+        m.style.removeProperty("border");
+        
+      }
+    }
+    function myFunction_3() {
+      var m = document.getElementById("articles_10_3");
+      var f = document.getElementById("flex_10_3");
+      var y =  document.getElementById("minus_3");
+      var z =  document.getElementById("plus_3");
+      var x = document.getElementById("para_of_10_3");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display= "inline";
+        z.style.display= "none";
+        f.style.color= "red";
+        m.style.color= "red";
+        m.style.border= "2px solid blue";
+      } else {
+        x.style.display = "none";
+        z.style.display= "inline";
+        y.style.display= "none";
+        f.style.removeProperty("color");
+        m.style.removeProperty("color");
+        m.style.removeProperty("border");
+      }
+    }
